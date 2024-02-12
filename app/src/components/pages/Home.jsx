d479eae1442cd204b3973aa73e304f84cfe94a52
Caroline-Mwangi <cwmwangi50@gmail.com>
2024-02-12 12:53:18 +0300

    feat: user home page
    - implement functionality to display users from api
    - implement functionality to display number of albums for the users
    - bootstrap styling for the layout

--- app/src/components/pages/Home.jsx
+++ app/src/components/pages/Home.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import AlbumCount from "../assets/AlbumCount";
+
 export default function Home() {
+  document.title = "SIL Assessment | Home";
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(response.data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
     <>
-      <p>Home page works!!</p>
+      <div class="row">
+        {users.map((user) => (
+          <div className="col-sm-12 col-md-4 col-lg-3 d-flex justify-content-center mb-5">
+            <div class="card w-75">
+              <h5 class="card-header">{user.name}</h5>
+              <div class="card-body">
+                <p class="card-title">
+                  <b>Username: </b>
+                  {user.username}
+                </p>
+                <p class="card-text">
+                  <AlbumCount userId={user.id} />
+                </p>
+                <a href="#">View User</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
